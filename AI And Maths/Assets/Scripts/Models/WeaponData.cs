f8e690aa03f2f8d40f7c32b6ca5cd971e2098715
Ayoub Laissaoui <w1602333@westminster.ac.uk>
2020-01-24 16:11:18 +0000

    fix Commited

--- AI And Maths/Assets/Scripts/Models/WeaponData.cs
+++ AI And Maths/Assets/Scripts/Models/WeaponData.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class WeaponData : MonoBehaviour
 {
+    public GameObject objectToIgnore;
     public int Damage = 10;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider == objectToIgnore.GetComponent<Collider2D>())
+        {
+            Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
+        }
+    }
 }
