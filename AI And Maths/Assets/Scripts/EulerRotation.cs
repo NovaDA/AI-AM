70f0ef3a279982a7e9bc01881ec579e0835d4b8d
Ayoub Laissaoui <w1602333@westminster.ac.uk>
2020-01-31 15:57:20 +0000

    Update Math And Ai

---
+++ AI And Maths/Assets/Scripts/EulerRotation.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulerRotation : MonoBehaviour, IRotation
+{
+    //One way to create the Gimbal lock effect:
+    //1) Set CurrentRotation X to 90 
+    //2) Set AnglesToRotate Z to 90 
+    //CurrentRotation = new Vector3 (90f, 0f, 0f);
+    //AnglesToRotate = new Vector3 (0f, 0f, 90f);
+
+    [SerializeField]
+    private Vector3 _currentRotation;
+    [SerializeField]
+    private Vector3 _anglesToRotate;
+
+    public Vector3 CurrentRotation
+    {
+        get
+        {
+            return _currentRotation;
+        }
+        set
+        {
+            _currentRotation = value;
+        }
+    }
+
+    public Vector3 AnglesToRotate {
+        get
+        {
+            return _anglesToRotate;
+        }
+        set
+        {
+            _anglesToRotate = value;
+        }
+    }
+
+    void Start()
+    {
+        transform.eulerAngles = CurrentRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentRotation += AnglesToRotate * Time.deltaTime;
+        transform.eulerAngles = CurrentRotation;
+    }
+}
---
+++ AI And Maths/Assets/Scripts/QuaternionRotation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionRotation : MonoBehaviour, IRotation
+{
+    [SerializeField]
+    private Vector3 _currentRotation;
+    [SerializeField]
+    private Vector3 _anglesToRotate;
+
+    public Vector3 CurrentRotation
+    {
+        get
+        {
+            return _currentRotation;
+        }
+        set
+        {
+            _currentRotation = value;
+        }
+    }
+
+    public Vector3 AnglesToRotate
+    {
+        get
+        {
+            return _anglesToRotate;
+        }
+        set
+        {
+            _anglesToRotate = value;
+        }
+    }
+
+    void Start()
+    {
+        Quaternion yRotation = Quaternion.AngleAxis(CurrentRotation.y, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis(CurrentRotation.x, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis(CurrentRotation.z, Vector3.forward);
+        transform.rotation = yRotation * xRotation * zRotation;
+    }
+
+    void Update()
+    {
+        Quaternion yRotation = Quaternion.AngleAxis(AnglesToRotate.y * Time.deltaTime, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis(AnglesToRotate.x * Time.deltaTime, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis(AnglesToRotate.z * Time.deltaTime, Vector3.forward);
+        transform.rotation = yRotation * xRotation * zRotation * transform.rotation;
+
+        CurrentRotation = transform.rotation.eulerAngles;
+    }
+
+}
